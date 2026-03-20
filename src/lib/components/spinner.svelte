35a9ef886370b3bb0d03cbcb2ea8a02b5e43699e
cleveng <cleveng@gmail.com>
2026-03-20 08:53:02 +0800

    perf: add title for spinner component

--- src/lib/components/spinner.svelte
+++ src/lib/components/spinner.svelte
@@ -5,9 +5,14 @@
 
   import type { ComponentProps } from 'svelte'
 
-  type Props = ComponentProps<typeof LoaderIcon>
+  type Props = ComponentProps<typeof LoaderIcon> & {
+    title?: string
+  }
 
-  let { class: className, ...restProps }: Props = $props()
+  let { class: className, title = '正在请求...', ...restProps }: Props = $props()
 </script>
 
-<LoaderIcon role="status" aria-label="Loading" class={cn('size-4 animate-spin', className)} {...restProps} />
+<div class="flex flex-col items-center justify-center space-y-1.5">
+  <LoaderIcon role="status" aria-label="Loading" class={cn('size-4 animate-spin', className)} {...restProps} />
+  <span class="text-sm">{title}</span>
+</div>
